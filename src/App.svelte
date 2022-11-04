260e779e58450b4b086b5915c69dc76a342c3ae1
Carlos Alvarez <carlosprade00@gmail.com>
2022-11-04 11:57:05 +0100

    TodoList

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,54 @@
 <script>
-	export let name;
+  export let name;
+  import Login from "./Login.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css"
+  />
+</svelte:head>
+
+<main class="content">
+  <h1>Welcome to {name}</h1>
+
+  <Login />
 </main>
 
+<!-- 
+	<script>
+  import Profile from "./Profile.svelte";
+  import Todos from "./Todos.svelte";
+
+  import { auth, googleProvider } from "./firebase";
+  import { authState } from "rxfire/auth";
+
+  let user;
+
+  const unsubscribe = authState(auth).subscribe((u) => (user = u));
+
+  function login() {
+    auth.signInWithPopup(googleProvider);
+  }
+</script> -->
+
+<!-- <section>
+  {#if user}
+    <Profile {...user} />
+    <button on:click={() => auth.signOut()}>Logout</button>
+    <hr />
+    <Todos uid={user.uid} />
+  {:else}
+    <button on:click={login}> Signin with Google </button>
+  {/if}
+</section> -->
+
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: purple;
+  }
+  main {
+    margin: 20px;
+  }
 </style>
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import "firebase/auth";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCHR3ezskT1JwQD58mKZHxz0BKok9V8NVk",
+  authDomain: "todolist-ce0b1.firebaseapp.com",
+  projectId: "todolist-ce0b1",
+  storageBucket: "todolist-ce0b1.appspot.com",
+  messagingSenderId: "874477453480",
+  appId: "1:874477453480:web:c525ae1b198ae060ba029d",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+export const db = firebase.firestore();
