4d08b80765aa41f136a74f804b4e29d81c2a29ed
Carlos Alvarez <carlosprade00@gmail.com>
2022-11-23 17:23:53 +0100

    inicio filtros matricula

--- src/Todos.svelte
+++ src/Todos.svelte
@@ -5,20 +5,30 @@
   import { startWith } from "rxjs/operators";
   import Content from "./Content.svelte";
   import Modal from "svelte-simple-modal";
+  import { async } from "rxjs";
 
   export let uid;
 
-  let text = "";
-  let matricula = "";
-  let km = "";
-  let modelo = "";
-  let color = "#000000";
-  
+  // let text = "";
+  // let matricula = "";
+  // let km = "";
+  // let modelo = "";
+  // let color = "#000000";
 
-  const query = db
+  let search;
+  let filteredQuery;
+
+  let query = db
     .collection("todos")
     .where("uid", "==", uid)
     .orderBy("created", "desc");
+    // .where("matricula", "==", "%54%");
+
+  async function filter(search) {
+    // funcionalidad de filtrar por matricula
+    query = query.where("matricula", "==", "%54%");
+    console.log(search);
+  }
 
   const todos = collectionData(query, "id").pipe(startWith([]));
 
@@ -32,23 +42,23 @@
     return querySnapshot.docs[0].id;
   }
 
-  function add() {
-    // Open Create Task Modal
-    db.collection("todos").add({
-      uid,
-      matricula,
-      km,
-      modelo,
-      color,
-      text,
-      complete: false,
-      created: Date.now(),
-    });
-    text = "";
-    matricula = "";
-    km = "";
-    modelo = "";
-  }
+  // function add() {
+  //   // Open Create Task Modal
+  //   db.collection("todos").add({
+  //     uid,
+  //     matricula,
+  //     km,
+  //     modelo,
+  //     color,
+  //     text,
+  //     complete: false,
+  //     created: Date.now(),
+  //   });
+  //   text = "";
+  //   matricula = "";
+  //   km = "";
+  //   modelo = "";
+  // }
 
   async function updateStatus(event) {
     const { newStatus, created } = event.detail;
@@ -63,7 +73,16 @@
   }
 </script>
 
+<!--
+Filtros para la tabla
+https://svelte.dev/repl/0429bd69748e44cdaeb8074c982f967d?version=3.41.0 
+-->
+
 <ul>
+  <div style="float: right;">
+    <input type="search" bind:value={search} placeholder="Search" />
+    <button on:click={filter(search)}>Search</button>
+  </div>
   <table>
     <tr>
       <th>Modelo</th>
